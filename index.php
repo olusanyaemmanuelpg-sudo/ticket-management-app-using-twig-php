3ff4ccabded0ba9a1abc6a6271e9027277f77c78
Olusanya Emmanuel <olusanyaemmanuelpg@gmailcom>
2025-10-28 16:39:57 +0100

    remove the error message

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ echo "PHP is working";
 require_once 'vendor/autoload.php';
 require_once __DIR__ . '/auth.php';
 
-session_start();
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
 $twig   = new \Twig\Environment($loader, ['cache' => false]);
